8db2021d114df34292644d2d09ff4c8b4038b0c9
Hywel <bonnefondhywel@gmail.com>
2023-02-06 15:11:37 +0100

    afficher vaisseau joueur

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const grid = document.querySelector('.grille');
+var playerPlace = 389;
 
-for (var i = 0; i < 400; i++) {
-    const square = document.createElement('div');
-    grid.appendChild(square);
+for (var i = 0; i < 400 ; i++) {
+    const grids = document.createElement('div');
+    grid.appendChild(grids);
 }
+
+const player = Array.from(document.querySelectorAll('.grille div'));
+
+player[playerPlace].classList.add('tireur');
