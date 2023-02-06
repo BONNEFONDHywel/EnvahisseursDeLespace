551c23f22aaceb84bf60baceb9bdf939fa93041d
Hywel <bonnefondhywel@gmail.com>
2023-02-06 14:21:54 +0100

    finalGrid

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const grid = document.querySelector('.grille');
 
-for (var i = 0; i < 225; i++) {
+for (var i = 0; i < 400; i++) {
     const square = document.createElement('div');
     grid.appendChild(square);
 }
