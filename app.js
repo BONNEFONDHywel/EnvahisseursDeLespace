a5743a6ba5f976f65900dd885dda9ae5da077398
Hywel <bonnefondhywel@gmail.com>
2023-02-07 22:20:54 +0100

    laser

--- app.js
+++ app.js
@@ -1,53 +1,93 @@
 const grid = document.querySelector('.grille');
 var playerPlace = 389;
+var playerLaser = playerPlace - 20;
 var width;
 var height;
 
 for (var i = 0; i < 400 ; i++) {
+
     const grids = document.createElement('div');
     grid.appendChild(grids);
+
 };
 
 const player = Array.from(document.querySelectorAll('.grille div'));
 
 player[playerPlace].classList.add('tireur');
 
+const laser = Array.from(document.querySelectorAll('.grille div'));
+
+function moveLaser() {
+    laser[playerLaser].classList.add('laser');
+    while (playerLaser > 9) {
+        laser[playerLaser].classList.remove('laser');
+        playerLaser -= 20;
+        laser[playerLaser].classList.add('laser');
+    }
+    laser[playerLaser].classList.remove('laser');
+}
+
 document.addEventListener("keydown", function(event) {
+
     switch(event.code){
+
         case "ArrowLeft":
+
+            moveLaser();
+
             player[playerPlace].classList.remove('tireur');
             playerPlace -= 1;
+
             if (playerPlace < 340 || playerPlace == 359 || playerPlace == 379) {
+
                 playerPlace += 1;
+
             };
+
             player[playerPlace].classList.add('tireur');
             break;
         
         case "ArrowRight":
+
             player[playerPlace].classList.remove('tireur');
             playerPlace += 1;
+
             if (playerPlace > 399 || playerPlace == 360 || playerPlace == 380) {
+
                 playerPlace -= 1;
+
             };
+
             player[playerPlace].classList.add('tireur');
             break;
 
         case "ArrowUp":
+
             player[playerPlace].classList.remove('tireur');
             playerPlace -= 20;
+
             if (playerPlace < 340) {
+
                 playerPlace += 20;
+
             };
+
             player[playerPlace].classList.add('tireur');
             break;
 
         case "ArrowDown":
+
             player[playerPlace].classList.remove('tireur');
             playerPlace += 20;
+
             if (playerPlace > 399) {
+
                 playerPlace -= 20;
             };
+
             player[playerPlace].classList.add('tireur');
             break;
+
     };
+
 });
